7468364003476826510ca41b3f308722ed4f9674
ethan.mcneal <ethan.mcneal@gmail.com>
2021-07-09 12:27:43 -0600

    toBeNull

--- functions.js
+++ functions.js
@@ -1,6 +1,7 @@
 const functions = {
     add: (num1, num2) => num1 + num2,
-
+    isNull: () => null,
+    
 }
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -7,3 +7,17 @@ test('adds 2 + 2 to equal 4', () => {
 test('adds 2 + 2 to NOT equal 5', () => {
     expect(functions.add(2, 2)).not.toBe(5)
 })
+
+
+
+
+//CHECK FOR TRUTHY AND FALSE VALUES
+//toBeNull === null
+//toBeUndefined === undefined
+//toBeDefined !== undefined
+//toBeTruthy === anything that an if statement treats as true
+//toBeFalsy === anything an if statement treats as false
+
+test('should be Null', () => {
+    expect(functions.isNull()).toBeNull()
+})
