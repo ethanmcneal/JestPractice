4fd5bf4314f4cf9a0d1b795e3bdd88a69b60bba8
ethan.mcneal <ethan.mcneal@gmail.com>
2021-07-09 12:24:10 -0600

    .not.toBe

--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,7 @@ const functions = require('./functions')
 test('adds 2 + 2 to equal 4', () => {
     expect(functions.add(2, 2)).toBe(4)
 })
+
+test('adds 2 + 2 to NOT equal 5', () => {
+    expect(functions.add(2, 2)).not.toBe(5)
+})
